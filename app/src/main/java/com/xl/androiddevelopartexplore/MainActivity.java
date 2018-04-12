c9711cd15bb96ce625e091515d70302d7e45676f
lu-xu <1510312433@qq.com>
2018-04-12 14:38:31 +0800

    develop 本地

--- app/src/main/java/com/xl/androiddevelopartexplore/MainActivity.java
+++ app/src/main/java/com/xl/androiddevelopartexplore/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Log.i(TAG, "onCreate: current pid ==.............>> " + android.os.Process.myPid());
+        Log.i(TAG, "这里是develop 的本地分支 " + android.os.Process.myPid());
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
